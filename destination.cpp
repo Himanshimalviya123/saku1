0a611d0f09cf87ae417093197457d9ccb72a6c58
Himanshimalviya123 <malviyahimanshi14@gmail.com>
2024-12-13 22:19:59 +0530

    update

---
+++ destination.cpp
@@ -0,0 +1,27 @@
+# include<iostream>
+using namespace std;
+int main(){
+int d;
+cout<<"enter your location 1. 2. 3. 4."; cin>>d;
+if(d==1)
+{cout<<"enter your destination";cin>>d;
+if(d==2)
+{cout<<"enter your destination";cin>>d;
+if(d==3)
+{cout<<"enter your destination";cin>>d;
+if(d==4)
+{cout<<"reached your dstination";}
+else{cout<<"invalid path";} }
+else if(d==4)
+{cout<<"reached your destination";}
+else{cout<<"invalid path"; }}
+else{ cout<<"invalid path";}}
+}
+ 
+ 
+ 
+ 
+ 
+ 
+    
+    
