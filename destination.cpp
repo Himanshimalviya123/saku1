3475e0a3029316680a27cdaf352bd734905fe7b8
Himanshimalviya123 <malviyahimanshi14@gmail.com>
2024-12-16 20:10:16 +0530

    update

--- destination.cpp
+++ destination.cpp
@@ -86,6 +86,55 @@ int main()
 
     }
 }
+#include<iostream>
+using namespace std;
+int main()
+{
+    for(int i=1;i<=5;++i)
+    {
+        for(int j=1;j<=i;++j)
+        
+        
+            {
+                cout<<i;
+            }
+            cout<<endl;
+        
+    }
+}
+#include<iostream>
+using namespace std;
+int main()
+{
+    for(int i=1;i<=5;++i)
+    {
+        for(int j=1;j<=i;++j)
+        
+        
+            {
+                cout<<j;
+            }
+            cout<<endl;
+        
+    }
+}#include<iostream>
+using namespace std;
+int main()
+{
+    int a=1;
+    for(int i=1;i<=4;++i)
+    {
+        for(int j=1;j<=i;++j)
+        
+        
+            {
+                cout<<a;
+                a++;
+            }
+            cout<<endl;
+        
+    }
+}
 }
  
  
