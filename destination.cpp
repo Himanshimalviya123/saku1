9081b7600f7f53b2c089f2d59b45367a94b1960a
Himanshimalviya123 <malviyahimanshi14@gmail.com>
2024-12-20 22:58:56 +0530

    update

--- destination.cpp
+++ destination.cpp
@@ -151,6 +151,56 @@ int main()
     }
     cout<<min;
 }
+// <========================arrray wwithout 3=========================>
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[]={1,2,3,4,5};
+    for(int i=0;i<5;++i)
+    {
+        if(arr[i]!=3)
+        {
+            cout<<arr[i];
+        }
+    }
+}
+// <===================comparision=======================>
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[]={1,2,3,1,2,1};
+    for(int i=0;i<5;++i)
+    {
+        for(int j=i+1;j<5;++j)
+        {
+        if(arr[i]==arr[j])
+        {
+            cout<<arr[i];
+        }
+    }
+}
+}
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[]={1,2,7,1,2,8,7,1,5,2};
+    for(int i=0;i<10;++i)
+    {
+    bool dup=false;
+    
+        for(int j=i+1;j<10;++j)
+        {
+        if(arr[i]==arr[j] )
+        {
+            
+            cout<<arr[i];
+        }
+    }
+}
+}
 }
  
  
