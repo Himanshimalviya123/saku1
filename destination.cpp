402539337c6f5fc7b79fb9b3c49138a5f2a37b6b
Himanshimalviya123 <malviyahimanshi14@gmail.com>
2024-12-21 22:39:37 +0530

    update

--- destination.cpp
+++ destination.cpp
@@ -254,6 +254,65 @@ if(dup )
 }
 }
 }
+// <==========================frequency============================>
+# include<iostream>
+using namespace std;
+int main()
+{
+    int arr[]={1,2,1,3,3,6,8,5,5,6,6,4};
+    for(int i=0;i<12;++i)
+    {
+        int count=1;
+        for(int j=i+1;j<12;++j)
+    {
+        if(arr[i]==arr[j])
+        {
+            count++;
+            arr[j]=-1;
+            
+        }
+    }
+    if(arr[i]!=-1)
+    {
+        cout<<"the freq of"<<arr[i]<<"is"<<count<<endl;    }
+    }
+}
+// <================reverse number==================>
+# include<iostream>
+using namespace std;
+int main()
+{
+    int arr[]={1,2,1,3,3,6};
+    for(int i=0;i<6/2;++i)
+    {
+       int temp;
+       temp=arr[i];
+       arr[i]=arr[6-i-1];
+       arr[6-i-1]=temp;
+    }
+     for(int i=0;i<6;++i)
+     {
+         cout<<arr[i];
+  
+    }
+}
+// <==================palindrown=====================>
+# include<iostream>
+using namespace std;
+int main()
+{
+    int arr[]={1,2,1,2,1};
+    for(int i=0;i<5/2;++i)
+ {
+     if(arr[i]!=arr[5-i-1])
+     {
+         cout<<"not pallin";
+         return 0;
+     }
+ }
+ cout<<"palindrown";
+      
+}
 }
  
  
